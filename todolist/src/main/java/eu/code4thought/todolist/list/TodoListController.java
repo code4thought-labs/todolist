acc32f1a3a3ad79c810b78e48f368a7e2d261eda
Orestis Konstantinidis <okonstantinidis@okonstantinidislaptop.sig.eu>
2020-04-30 18:34:44 +0300

    Starting the creation of endpoints

--- todolist/src/main/java/eu/code4thought/todolist/list/TodoListController.java
+++ todolist/src/main/java/eu/code4thought/todolist/list/TodoListController.java
@@ -1,16 +1,26 @@
 package eu.code4thought.todolist.list;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.PathVariable;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todolist")
 public class TodoListController {
+
     @GetMapping("/{description}")
     public TodoList getOne(@PathVariable String description) {
-        return new TodoList(description);
+        // return new TodoList(description);
         // Connect to: http://localhost:8080/todolist/123
         // or Curl: `curl localhost:8080/todolist/123`
+        return null;
+    }
+
+    @GetMapping("/")
+    public TodoList getAll() {
+        // return new TodoList(description);
+        // Connect to: http://localhost:8080/todolist/123
+        // or Curl: `curl localhost:8080/todolist/123`
+        return null;
     }
 }
