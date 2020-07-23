1598bd69200583d86e004e9c207553c30bbdfcf5
Orestis Konstantinidis <okonstantinidis@okonstantinidislaptop.sig.eu>
2020-07-23 17:58:26 +0300

    equals() and hashCode() implementation

--- todolist/src/main/java/eu/code4thought/todolist/list/ListItem.java
+++ todolist/src/main/java/eu/code4thought/todolist/list/ListItem.java
@@ -3,22 +3,23 @@ package eu.code4thought.todolist.list;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class ListItem {
     private String description = "";
     private int id;
+    private TodoList parent;
     private Date last_updated = Calendar.getInstance().getTime();
     private boolean completed = false;
-    private static int next_id = 0;
 
     public ListItem() {
-        this("This is the default description of this task.");
+        this("This is the default description of this task.", new TodoList("fosterParent"));
     }
 
-    public ListItem(String description) {
+    public ListItem(String description, TodoList parent) {
         this.description = description;
-        this.id = next_id;
-        next_id++;
+        this.id = System.identityHashCode(System.nanoTime());
+        this.parent = parent;
         this.last_updated = Calendar.getInstance().getTime();
     }
 
@@ -27,7 +28,7 @@ public class ListItem {
     }
 
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
     public void setDescription(String description) {
@@ -40,6 +41,27 @@ public class ListItem {
     }
 
     public int getId() {
-        return id;
+        return this.id;
+    }
+
+    public TodoList getParent(){
+        return this.parent;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.parent.getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        ListItem other = (ListItem) obj;
+        return Objects.equals(id, other.getId());
     }
 }
--- todolist/src/main/java/eu/code4thought/todolist/list/TodoList.java
+++ todolist/src/main/java/eu/code4thought/todolist/list/TodoList.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name="todolist")
@@ -26,12 +27,12 @@ public class TodoList {
 
     public TodoList(String name) {
         this.name = name;
-        this.id = System.identityHashCode(System.nanoTime());
+        this.id = System.identityHashCode(System.nanoTime()); // Not beautiful, please make @GeneratedValue work.
         this.items = new ArrayList<>();
     }
 
     public ListItem createItem(String description) {
-        ListItem new_item =  new ListItem(description);
+        ListItem new_item =  new ListItem(description, this);
 //      alternatively  item's father = this list
         this.items.add(new_item);
         return new_item;
@@ -58,12 +59,31 @@ public class TodoList {
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
+
+    public int getId() { return this.id;}
 
     public void move(ListItem item, TodoList to) {
         to.createItem(item.getDescription());
         this.remove(item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TodoList other = (TodoList) obj;
+        return Objects.equals(id, other.getId());
     }
 
 }
